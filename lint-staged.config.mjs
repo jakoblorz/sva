6165c48736776d80ad2930e7146cf86821f09cd5
Jakob Lorz <jakoblorz@users.noreply.github.com>
2023-05-08 23:09:22 +0200

    fix: some structural refactorings

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,6 +1,6 @@
 export default {
   "*": (filenames) =>
-    `prettier --ignore-unknown --no-error-on-unmatched-pattern --write ${filenames
+    `prettier --ignore-unknown --no-error-on-unmatched-pattern --plugin=./node_modules/prettier-plugin-organize-imports --write ${filenames
       .map((filename) => `'${filename}'`)
       .join(" ")}`,
   "src/**/*.{js,jsx,ts,tsx}": () => "npm run lint:tsc",
